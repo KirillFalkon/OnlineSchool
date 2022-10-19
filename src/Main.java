6a4226faf7b2c82b230ee080a22c997cc9f5e50d
Kyrylo_Falko <kirill1.falko@gmail.com>
2022-10-19 18:45:04 +0300

    add else one object of Lecture

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,9 @@ public class Main {
         lectureService.createLecture("1", new Homework("1", 1), new AdditionalMaterials("1", 1), 1);
         lectureService.createLecture("1", new Homework("1", 1), new AdditionalMaterials("1", 1), 1);
 
+        LectureService lectureService1 = new LectureService();
+        lectureService1.createLecture("1", new Homework("1", 1), new AdditionalMaterials("1", 1), 1);
+
         System.out.println(lectureService.countLecture);
     }
 }
